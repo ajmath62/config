3abaebe965b9e09877fc14fc313afe32228cabb8
Aaron Klein <ajmath62@gmail.com>
2018-11-19 17:56:36 -0500

    Added more commit hooks

--- .git-templates/hooks/hook_helpers.py
+++ .git-templates/hooks/hook_helpers.py
@@ -1,6 +1,15 @@
 """ Define some helper methods for git hooks """
-
+import subprocess
 import sys
+
+import six
+
+
+class PY34CompletedProcess:
+    """ Mock object that behaves similarly to Python 3.5+ subprocess.CompletedProcess """
+    def __init__(self, returncode=None, stdout=None):
+        self.returncode = returncode
+        self.stdout = stdout
 
 
 class RemapStdinToTTY:
@@ -24,6 +33,26 @@ def ignore_or_exit():
     with RemapStdinToTTY():
         if not input('Ignore and commit anyways? (y/n) ').lower().startswith('y'):
             exit(1)
+
+
+def subprocess_run(*args, **kwargs):
+    """ Function that works similarly to Python 3.5+ subprocess.run
+    
+    It is not a perfect mapping, but it can be extended as needed
+    """
+    if six.PY34:
+        if kwargs.pop('check', False):
+            if kwargs.pop('stdout', None) == subprocess.PIPE:
+                stdout = subprocess.check_output(*args, **kwargs)
+                return PY34CompletedProcess(stdout=stdout)
+            else:
+                returncode = subprocess.check_call(*args, **kwargs)
+                return PY34CompletedProcess(returncode=returncode)
+        else:
+            returncode = subprocess.call(*args, **kwargs)
+            return PY34CompletedProcess(returncode=returncode)
+    else:
+        return subprocess.run(*args, **kwargs)
 
 
 PRINT_STATEMENTS = ('print', 'puts', 'console')
